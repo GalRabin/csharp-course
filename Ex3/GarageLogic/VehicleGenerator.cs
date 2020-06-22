a84bc4c4fb95039de6831cbf341e65238a09a5e1
dean <deanbm21@gmail.com>
2020-06-22 14:48:01 +0300

    dean finished including generator

--- Ex3/GarageLogic/VehicleGenerator.cs
+++ Ex3/GarageLogic/VehicleGenerator.cs
@@ -16,34 +16,19 @@ namespace GarageLogic
             switch (i_VehicleType)
             {
                 case Enums.eVehicleType.FuelMotorcycle:
-                    vehicle = new FuelMotorcycle((Customer)i_Properties["Customer"], (string)i_Properties["Model Name"],
-                         (string)i_Properties["License Number"], wheels,
-                         (Engines.FuelEngine)i_Properties["Fuel Engine"],
-                         (Enums.eMotorcycleLicenseTypes)i_Properties["License Type"], (int)i_Properties["Engine Volume"]);
+                    vehicle = createFuelMotorcycle(i_Properties, wheels);
                     break;
                 case Enums.eVehicleType.ElectricMotorcycle:
-                    vehicle = new ElectricMotorcycle((Customer)i_Properties["Customer"], (string)i_Properties["Model Name"],
-                        (string)i_Properties["License Number"], wheels,
-                        (Engines.ElectricEngine)i_Properties["Electric Engine"],
-                        (Enums.eMotorcycleLicenseTypes)i_Properties["License Type"], (int)i_Properties["Engine Volume"]);
+                    vehicle = createElectricMotorcycle(i_Properties, wheels);
                     break;
                 case Enums.eVehicleType.FuelCar:
-                    vehicle = new FuelCar((Customer)i_Properties["Customer"], (string)i_Properties["Model Name"],
-                              (string)i_Properties["License Number"], wheels,
-                              (Engines.FuelEngine)i_Properties["Fuel Engine"],
-                              (Enums.eCarColors)i_Properties["Car Color"], (int)i_Properties["Number Of Doors"]);
+                    vehicle = createFuelCar(i_Properties, wheels);
                     break;
                 case Enums.eVehicleType.ElectricCar:
-                    vehicle = new ElectricCar((Customer)i_Properties["Customer"], (string)i_Properties["Model Name"],
-                               (string)i_Properties["License Number"], wheels,
-                               (Engines.ElectricEngine)i_Properties["Electric Engine"],
-                               (Enums.eCarColors)i_Properties["Car Color"], (int)i_Properties["Number Of Doors"]);
+                    vehicle = createElectricCar(i_Properties, wheels);
                     break;
                 case Enums.eVehicleType.FuelTruck:
-                    vehicle = new FuelTruck((Customer)i_Properties["Customer"], (string)i_Properties["Model Name"],
-                              (string)i_Properties["License Number"], wheels,
-                              (Engines.FuelEngine)i_Properties["Fuel Engine"],
-                              (float)i_Properties["Cargo Volume"], (bool)i_Properties["Dangerous Cargo"]);
+                    vehicle = createFuelTruck(i_Properties, wheels);
                     break;
             }
 
@@ -63,6 +48,46 @@ namespace GarageLogic
             }
 
             return wheels;
+        }
+        private static FuelMotorcycle createFuelMotorcycle(Dictionary<string, object> i_Properties, List<Wheel> i_Wheels)
+        {
+
+            return new FuelMotorcycle((Customer)i_Properties["Customer"], (string)i_Properties["Model Name"],
+                         (string)i_Properties["License Number"], i_Wheels,
+                         (Engines.FuelEngine)i_Properties["Fuel Engine"],
+                         (Enums.eMotorcycleLicenseTypes)i_Properties["License Type"], (int)i_Properties["Engine Volume"]);
+        }
+        private static ElectricMotorcycle createElectricMotorcycle(Dictionary<string, object> i_Properties, List<Wheel> i_Wheels)
+        {
+
+            return new ElectricMotorcycle((Customer)i_Properties["Customer"], (string)i_Properties["Model Name"],
+                        (string)i_Properties["License Number"], i_Wheels,
+                        (Engines.ElectricEngine)i_Properties["Electric Engine"],
+                        (Enums.eMotorcycleLicenseTypes)i_Properties["License Type"], (int)i_Properties["Engine Volume"]);
+        }
+        private static FuelCar createFuelCar(Dictionary<string, object> i_Properties, List<Wheel> i_Wheels)
+        {
+
+            return new FuelCar((Customer)i_Properties["Customer"], (string)i_Properties["Model Name"],
+                              (string)i_Properties["License Number"], i_Wheels,
+                              (Engines.FuelEngine)i_Properties["Fuel Engine"],
+                              (Enums.eCarColors)i_Properties["Car Color"], (int)i_Properties["Number Of Doors"]);
+        }
+        private static ElectricCar createElectricCar(Dictionary<string, object> i_Properties, List<Wheel> i_Wheels)
+        {
+
+            return new ElectricCar((Customer)i_Properties["Customer"], (string)i_Properties["Model Name"],
+                               (string)i_Properties["License Number"], i_Wheels,
+                               (Engines.ElectricEngine)i_Properties["Electric Engine"],
+                               (Enums.eCarColors)i_Properties["Car Color"], (int)i_Properties["Number Of Doors"]);
+        }
+        private static FuelTruck createFuelTruck(Dictionary<string, object> i_Properties, List<Wheel> i_Wheels)
+        {
+
+            return new FuelTruck((Customer)i_Properties["Customer"], (string)i_Properties["Model Name"],
+                              (string)i_Properties["License Number"], i_Wheels,
+                              (Engines.FuelEngine)i_Properties["Fuel Engine"],
+                              (float)i_Properties["Cargo Volume"], (bool)i_Properties["Dangerous Cargo"]);
         }
     }
 }

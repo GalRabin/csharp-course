7896d28162906b2ac2589e4614ef9b9847f2a196
grabin <grabin@paloaltonetworks.com>
2020-07-07 04:01:47 -0700

    fixing all

---
+++ Ex5/GameUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameUI
+{
+    class Program
+    {
+        public static void Main()
+        {
+            new FormLogin().ShowDialog();
+        }
+    }
+}

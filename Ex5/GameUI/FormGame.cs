d2b44b9a96d5e3e216b76c44d8d21e19cf5efde3
grabin <grabin@paloaltonetworks.com>
2020-07-07 01:31:52 -0700

    Starting UI

--- Ex5/GameUI/FormGame.cs
+++ Ex5/GameUI/FormGame.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace GameUI
 {
-    public partial class Manager : Form
+    public partial class FormGame : Form
     {
-        public Manager()
+        public FormGame()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- Ex5/GameUI/LoginForm.cs
+++ Ex5/GameUI/LoginForm.cs
@@ -10,19 +10,24 @@ using System.Windows.Forms;
 
 namespace GameUI
 {
-    public partial class Form1 : Form
+    public partial class LoginForm : Form
     {
-        public Form1()
+        public LoginForm()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
         {
 
         }
